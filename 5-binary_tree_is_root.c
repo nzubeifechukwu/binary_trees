b6ee9bd920a6a3aabe47e55a829cf9509477f31a
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-03-03 08:08:29 -0800

    Update 5-binary_tree_is_root.c

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -4,7 +4,7 @@
  * binary_tree_is_root - Checks if a given node is a root
  * @node: Points to the node to check
  *
- * Return 1 if @node is a root, 0 otherwise or if @node is NULL
+ * Return: 1 if @node is a root, 0 otherwise or if @node is NULL
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
