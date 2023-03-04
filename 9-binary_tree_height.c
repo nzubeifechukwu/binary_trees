ed203ab3feab0510b742b0ab1e59cd2d0c9ca68c
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-03-04 01:51:11 -0800

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -30,5 +30,8 @@ size_t binary_tree_height_(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
+
 	return (binary_tree_height_(tree) - 1);
 }
