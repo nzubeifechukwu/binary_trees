0a8f0019fab6c7670aefdc5371d6a7c1d465f480
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-03-04 08:11:06 -0800

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,7 +9,7 @@ void binary_tree_nodes_(const binary_tree_t *tree, size_t *nodes)
 {
 	if (tree == NULL)
 		return;
-	
+
 	if (tree->left != NULL || tree->right != NULL)
 		(*nodes)++;
 
@@ -20,6 +20,8 @@ void binary_tree_nodes_(const binary_tree_t *tree, size_t *nodes)
 /**
  * binary_tree_nodes - Counts no of nodes of binary tree with at least 1 child
  * @tree: Points to the root node of the tree
+ *
+ * Return: Number of nodes with at least 1 child
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
