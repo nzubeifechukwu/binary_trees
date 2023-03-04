3609fe6919c1dff6c99e0f5eaf57f31b4f751953
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-03-04 08:08:04 -0800

    Add 13-binary_tree_nodes.c and update binary_trees.h

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes_ - Helper function for the binary_tree_nodes function
+ * @tree: Points to the root node of a binary tree
+ * @nodes: Stores the number of nodes in the binary tree
+ */
+void binary_tree_nodes_(const binary_tree_t *tree, size_t *nodes)
+{
+	if (tree == NULL)
+		return;
+	
+	if (tree->left != NULL || tree->right != NULL)
+		(*nodes)++;
+
+	binary_tree_nodes_(tree->left, nodes);
+	binary_tree_nodes_(tree->right, nodes);
+}
+
+/**
+ * binary_tree_nodes - Counts no of nodes of binary tree with at least 1 child
+ * @tree: Points to the root node of the tree
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	binary_tree_nodes_(tree, &nodes);
+
+	return (nodes);
+}
